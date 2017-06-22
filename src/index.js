c8e8f406bcd60378d9989a00e88e69801c31196d
geekplux <geekplux@qq.com>
2017-06-22 14:23:26 +0800

    improve style

--- src/index.js
+++ src/index.js
@@ -1,27 +1,24 @@
 import tracking from 'tracking';
 import 'tracking-face';
+import './style.css';
 
-window.onload = function () {
-  const canvas = document.getElementById('canvas');
-  const context = canvas.getContext('2d');
-  const tracker = new tracking.ObjectTracker('face');
-  tracker.setInitialScale(4);
-  tracker.setStepSize(2);
-  tracker.setEdgesDensity(0.1);
-  tracking.track('#video', tracker, { camera: true });
-  tracker.on('track', function (event) {
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    event.data.forEach(function (rect) {
-      context.strokeStyle = '#a64ceb';
-      context.strokeRect(rect.x, rect.y, rect.width, rect.height);
-      context.font = '11px Helvetica';
-      context.fillStyle = '#fff';
-      context.fillText('x: ' + rect.x + 'px', rect.x + rect.width + 5, rect.y + 11);
-      context.fillText('y: ' + rect.y + 'px', rect.x + rect.width + 5, rect.y + 22);
-    });
+const canvas = document.getElementById('canvas');
+const context = canvas.getContext('2d');
+const tracker = new tracking.ObjectTracker('face');
+tracker.setInitialScale(4);
+tracker.setStepSize(2);
+tracker.setEdgesDensity(0.1);
+tracking.track('#video', tracker, { camera: true });
+tracker.on('track', (event) => {
+  console.log(event);
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  event.data.forEach((rect) => {
+    context.strokeStyle = '#a64ceb';
+    context.lineWidth = 10;
+    context.strokeRect(rect.x, rect.y, rect.width, rect.height);
+    context.font = '11px Helvetica';
+    context.fillStyle = '#fff';
+    context.fillText(`x: ${rect.x}px`, rect.x + rect.width + 5, rect.y + 11);
+    context.fillText(`y: ${rect.y}px`, rect.x + rect.width + 5, rect.y + 22);
   });
-  // var gui = new dat.GUI();
-  // gui.add(tracker, 'edgesDensity', 0.1, 0.5).step(0.01);
-  // gui.add(tracker, 'initialScale', 1.0, 10.0).step(0.1);
-  // gui.add(tracker, 'stepSize', 1, 5).step(0.1);
-};
+});
