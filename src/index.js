afc80cae2185196773bdf92c16aad1aa1692e85c
geekplux <geekplux@qq.com>
2017-06-29 15:43:08 +0800

    enable rear camera of mobile phone to get video stream

--- src/index.js
+++ src/index.js
@@ -2,6 +2,66 @@ import tracking from 'tracking';
 import 'tracking-face';
 import './style.css';
 
+var videoElement = document.querySelector('video');
+var audioSelect = document.querySelector('select#audioSource');
+var videoSelect = document.querySelector('select#videoSource');
+
+navigator.mediaDevices.enumerateDevices()
+  .then(gotDevices).then(getStream).catch(handleError);
+
+audioSelect.onchange = getStream;
+videoSelect.onchange = getStream;
+
+function gotDevices (deviceInfos) {
+  for (let i = 0; i !== deviceInfos.length; ++i) {
+    const deviceInfo = deviceInfos[i];
+    const option = document.createElement('option');
+    option.value = deviceInfo.deviceId;
+    if (deviceInfo.kind === 'audioinput') {
+      option.text = deviceInfo.label ||
+        'microphone ' + (audioSelect.length + 1);
+      audioSelect.appendChild(option);
+    } else if (deviceInfo.kind === 'videoinput') {
+      option.text = deviceInfo.label || 'camera ' +
+        (videoSelect.length + 1);
+      videoSelect.appendChild(option);
+    } else {
+      console.log('Found ome other kind of source/device: ', deviceInfo);
+    }
+  }
+}
+
+function getStream () {
+  if (window.stream) {
+    window.stream.getTracks().forEach((track) => track.stop());
+  }
+
+  var constraints = {
+    audio: {
+      optional: [{
+        sourceId: audioSelect.value
+      }]
+    },
+    video: {
+      optional: [{
+        sourceId: videoSelect.value
+      }]
+    }
+  };
+
+  navigator.mediaDevices.getUserMedia(constraints)
+    .then(gotStream).catch(handleError);
+}
+
+function gotStream (stream) {
+  window.stream = stream; // make stream available to console
+  videoElement.srcObject = stream;
+}
+
+function handleError (error) {
+  console.log('Error: ', error);
+}
+
 /**
  * face tracking
  */
